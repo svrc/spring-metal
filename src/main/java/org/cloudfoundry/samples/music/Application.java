8741ef136efaad7eb9023ad09da3a1f76683f97b
dekel tankel <dekel@vmware.com>
2024-03-26 08:36:41 -0700

    add fips

--- src/main/java/org/cloudfoundry/samples/music/Application.java
+++ src/main/java/org/cloudfoundry/samples/music/Application.java
@@ -7,7 +7,6 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class Application {
 
     public static void main(String[] args) {

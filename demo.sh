9bf7610bae99e34b7039df70f61f77bc13c8c58c
dekel tankel <dekel@vmware.com>
2024-04-09 08:42:47 -0700

    modify service names and readme

--- demo.sh
+++ demo.sh
@@ -4,7 +4,6 @@ case $1 in
 create)
     cf create-service genai-service shared-ai-plan dekt-genai
     cf create-service postgres on-demand-postgres-db dekt-secret-sauce
-    #cf create-service postgres on-demand-postgres-small dekt-secret-sauce 
     ;;
 delete)
     cf delete-service dekt-genai -f

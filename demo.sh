e18b6bdc4d51162b085b596cf1f1346f49175d59
dekel tankel <dekel@vmware.com>
2024-08-15 15:45:33 +0000

    modify demo.sh and readme to support separate caht and embedding genai plans

--- demo.sh
+++ demo.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
-PGVECTOR_SERVICE_NAME="embeddings"
-GENAI_SERVICE_NAME="genai-enpoint"
+PGVECTOR_SERVICE_NAME="vector-db"
+PGVECTOR_PLAN_NAME="on-demand-postgres-db"
+
+GENAI_CHAT_SERVICE_NAME="genai-chat" # must be identical to runtime-configs/tpcf/manifest.yaml
+GENAI_CHAT_PLAN_NAME="meta-llama/Meta-Llama-3-8B-Instruct" # plan must have chat capabilty
+
+GENAI_EMBEDDINGS_SERVICE_NAME="genai-embed" # must be identical to runtime-configs/tpcf/manifest.yaml
+GENAI_EMBEDDINGS_PLAN_NAME="nomic-embed-text" # plan must have Embeddings capabilty
+
 
 case $1 in
 cf)
-    cf create-service postgres on-demand-postgres-db $PGVECTOR_SERVICE_NAME #-c '{"svc_gw_enable": true, "router_group": "default-tcp", "external_port": 1025}' -w
+    cf create-service postgres $PGVECTOR_PLAN_NAME $PGVECTOR_SERVICE_NAME #-c '{"svc_gw_enable": true, "router_group": "default-tcp", "external_port": 1025}' -w
     echo
 	printf "Waiting for service $PGVECTOR_SERVICE_NAME to create."
 	while [ `cf services | grep 'in progress' | wc -l | sed 's/ //g'` != 0 ]; do
@@ -15,7 +22,14 @@ cf)
 	echo
 	echo "$PGVECTOR_SERVICE_NAME creation completed."
 	echo
-    cf create-service genai shared $GENAI_SERVICE_NAME #-c '{"svc_gw_enable": true, "router_group": "default-tcp", "external_port": 1025}' -w
+ 
+	echo
+	echo "Creating $GENAI_CHAT_SERVICE_NAME and $GENAI_EMBEDDINGS_SERVICE_NAME GenAI services ..."
+	echo
+
+    cf create-service genai $GENAI_CHAT_PLAN_NAME $GENAI_CHAT_SERVICE_NAME #-c '{"svc_gw_enable": true, "router_group": "default-tcp", "external_port": 1025}' -w
+    cf create-service genai $GENAI_EMBEDDINGS_PLAN_NAME $GENAI_EMBEDDINGS_SERVICE_NAME #-c '{"svc_gw_enable": true, "router_group": "default-tcp", "external_port": 1025}' -w
+ 
     cf push -f runtime-configs/tpcf/manifest.yml 
     ;;
 k8s)
@@ -31,7 +45,8 @@ k8s)
     ;;
 cleanup)
     cf delete-service $PGVECTOR_SERVICE_NAME -f
-    cf delete-service $GENAI_SERVICE_NAME -f
+    cf delete-service $GENAI_CHAT_SERVICE_NAME -f
+    cf delete-service $GENAI_EMBEDDINGS_SERVICE_NAME -f
     cf delete dekt-metal -f
     kubectl delete -f .tanzu/services
     ;;
